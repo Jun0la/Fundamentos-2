07fc189ec66a9d8f7559e629c8d05c0ff1297fc6
Jun0la <contatojjunola@gmail.com>
2023-05-24 00:07:34 -0300

    Extensions

---
+++ ARCHIVES/DateTime Extensions/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(1998, 1, 21, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+
+            string s1 = "good morning folkz";
+
+            Console.WriteLine(s1.Cut(10));
+        }
+    }
+}
